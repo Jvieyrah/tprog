604134da08d11c84e49f978bfb651064c502edc1
jvieirah <me.contrate.aero@gmail.com>
2025-09-15 20:35:53 -0300

    exercicios 3 e 4

--- src/DesafioAgendamento.java
+++ src/DesafioAgendamento.java
@@ -6,14 +6,18 @@ public class DesafioAgendamento {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
         LocalDateTime dataEvento = LocalDateTime.of(2025, 10, 15, 14, 0);
     ZonedDateTime spTimeZone = ZonedDateTime.of(dataEvento, ZoneId.of("America/Sao_Paulo"));
+    System.out.println(ZoneId.of("America/Sao_Paulo"));
 
     System.out.println("Data e hora em São Paulo: " + formatter.format(spTimeZone));
         ZonedDateTime nyTimeZone = spTimeZone.withZoneSameInstant(ZoneId.of("America/New_York"));
+        System.out.println(ZoneId.of("America/New_York"));
     System.out.println("Data e hora em Nova York: " + formatter.format(nyTimeZone));
         ZonedDateTime tokyoTimeZone = spTimeZone.withZoneSameInstant(ZoneId.of("Asia/Tokyo"));
+        System.out.println(ZoneId.of("Asia/Tokyo"));
     System.out.println("Data e hora em Tóquio: " + formatter.format(tokyoTimeZone));
         ZonedDateTime londonTimeZone = spTimeZone.withZoneSameInstant(ZoneId.of("Europe/London"));
     System.out.println("Data e hora em Londres: " + formatter.format(londonTimeZone));
+        System.out.println(ZoneId.of("Europe/London"));
   }
 
 }
--- src/exercicios.java
+++ src/exercicios.java
@@ -1,5 +1,6 @@
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 public class exercicios {
   public static void main(String[] args) {
 
@@ -18,6 +19,33 @@ public class exercicios {
     System.out.println("Hora atual: " + horaAtual);
     LocalTime novaHora = horaAtual.plusHours(2).plusMinutes(30);
     System.out.println("Nova hora: " + novaHora);
-  }
 
+    //EXERCICIO 3
+
+    String dataNascinto2 = "16/02/1994";
+    String dataNascinto3 = "16/02/2004";
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    LocalDate dataNascinto = LocalDate.parse(dataNascinto2, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    LocalDate dataNascinto1 = LocalDate.parse(dataNascinto3, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    Duration diff = Duration.between(dataNascinto.atStartOfDay(), dataNascinto1.atStartOfDay());
+    System.out.println("Diferença em dias: " + diff.toDays());
+    System.out.println("Diferença em meses: " + (diff.toDays() / 30));
+    System.out.println("Diferença em anos: " + (diff.toDays() / 365));
+
+    //EXERCICIO 4
+    Scanner scanner = new Scanner(System.in);
+    LocalDateTime horaAtual1 = LocalDateTime.now();
+    System.out.println("pressione enter para calcular o tempo que passou...");
+    scanner.nextLine();
+    LocalDateTime novaHora1 = LocalDateTime.now();
+    Duration diff1 = Duration.between(horaAtual1, novaHora1);
+    System.out.println("Tempo que passou em segundos em nanossegundos: " + diff1.getSeconds() + " segundos e " + diff1.getNano() + " nanossegundos");
+
+
+
+
+
+  }
 }
